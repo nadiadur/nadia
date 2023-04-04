70b535dcc090c28a4a62e51d5a5433d1738eb990
Nadia Durrotul <nadia.durotul@gmail.com>
2023-04-04 09:51:40 +0700

    baru8

--- zakat.js
+++ zakat.js
@@ -5,12 +5,12 @@
             document.getElementById("harta").innerHTML=total;
             var persen=0.025;
             var bayar;
-            if(total>0){
-                bayar=total*dis;
-                document.getElementById("tot").innerHTML=bayar;
+            if(harta>0){
+                bayar=harta*persen;
+                document.getElementById("bayar").innerHTML=bayar;
                 document.getElementById("persen").innerHTML="2.5%";
             }else{
-                document.getElementById("tot").innerHTML=total;
+                document.getElementById("bayar").innerHTML=harta;
                 document.getElementById("persen").innerHTML="0%";
             }
         
